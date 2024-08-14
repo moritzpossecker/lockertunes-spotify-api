4623a908ad730c1b0382510c5ef06cf766d7bdd1
Moritz Poßecker <M.Possecker@deskcenter.com>
2024-08-14 15:22:02 +0200

    add get_track_list for playlist fucntion

--- spotify_modules/playlist_getter.py
+++ spotify_modules/playlist_getter.py
@@ -1,5 +1,21 @@
 import spotipy
 from models.playlist import Playlist
+from models.track import Track
+from track_searcher import create_track_element
+
+
+def get_track_list(playlist_id: str, access_token: str) -> list[Track] | None:
+    spotify_object = spotipy.Spotify(auth=access_token)
+    if playlist_id is None:
+        return None
+
+    results = spotify_object.playlist_items(playlist_id, limit=None)
+
+    track_list = []
+    for item in results['items']:
+        track = item['track']
+        track_list.append(create_track_element(track))
+    return track_list
 
 
 def get_playlists(access_token: str, user_id: str) -> list[Playlist]:
@@ -14,17 +30,17 @@ def get_playlists(access_token: str, user_id: str) -> list[Playlist]:
 
     playlists_result = []
     for playlist in playlists:
-        image_url = None
-        if playlist["images"]:
-            if playlist["images"][0]:
-                image_url = playlist["images"][0]["url"]
+        try:
+            image_url = playlist["images"][0]["url"]
+        except IndexError:
+            image_url = None
         playlists_result.append(Playlist(
             name=playlist["name"],
             description=playlist["description"],
             public=playlist["public"],
             collaborative=playlist["collaborative"],
             id=playlist["id"],
-            image_url=image_url,
+            image_url=image_url
         ))
 
     return playlists_result
--- spotify_modules/track_searcher.py
+++ spotify_modules/track_searcher.py
@@ -2,13 +2,22 @@ import spotipy
 from models.track import Track
 
 
+def create_track_element(track) -> Track:
+    try:
+        image_url = track['album']['images'][0]['url']
+    except IndexError:
+        image_url = None
+
+    return Track(track['uri'], track['name'], track['artists'][0]['name'],
+                 track['duration_ms'], image_url)
+
+
 def search_track_by_name(access_token: str, track_name: str, country: str) -> Track | None:
     spotify_object = spotipy.Spotify(auth=access_token)
     result = spotify_object.search(q=track_name, type='track', limit=1, market=country)
 
     if result['tracks']['items']:
         track = result['tracks']['items'][0]
-        return Track(track['uri'], track['name'], track['artists'][0]['name'],
-                     track['duration_ms'], track['album']['images'][0]['url'])
+        return create_track_element(track)
     else:
         return None
